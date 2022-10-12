45aa720f2428f48da314b310d491b2e527a478d7
Mitch Eatough <meatough@leagueapps.com>
2022-10-12 12:56:23 -0700

    Use cypress-grep style tag search

--- src/tagged.js
+++ src/tagged.js
@@ -1,28 +1,16 @@
 const { addCounts } = require('./count')
 
-function arraysOverlap(a, b) {
-  if (!Array.isArray(a) || !Array.isArray(b)) {
-    return false
-  }
-
-  if (a.length < b.length) {
-    return a.some((item) => b.includes(item))
-  } else {
-    return b.some((item) => a.includes(item))
-  }
-}
-
 // note: modifies the tests in place
 function pickTaggedTests(tests, tag) {
   if (!Array.isArray(tests)) {
     return false
   }
-  const tags = Array.isArray(tag) ? tag : [tag]
+  const tags = parseTagsGrep(tag)
   const filteredTests = tests.filter((test) => {
     if (test.type === 'test') {
-      return test.tags && arraysOverlap(test.tags, tags)
+      return shouldTestRunTags(tags, test.tags)
     } else if (test.type === 'suite') {
-      if (test.tags && arraysOverlap(test.tags, tags)) {
+      if (shouldTestRunTags(tags, test.tags)) {
         return true
       }
 
@@ -64,8 +52,87 @@ function pickTaggedTestsFrom(json, tag) {
   return result
 }
 
+/**
+ * Parses tags to grep for.
+ * @param {string} s Tags string like "@tag1+@tag2"
+ */
+function parseTagsGrep(s) {
+  if (!s) {
+    return []
+  }
+
+  const explicitNotTags = []
+
+  // top level split - using space or comma, each part is OR
+  const ORS = s
+    .split(/[ ,]/)
+    // remove any empty tags
+    .filter(Boolean)
+    .map((part) => {
+      // now every part is an AND
+      if (part.startsWith('--')) {
+        explicitNotTags.push({
+          tag: part.slice(2),
+          invert: true,
+        })
+        return
+      }
+      const parsed = part.split('+').map((tag) => {
+        if (tag.startsWith('-')) {
+          return {
+            tag: tag.slice(1),
+            invert: true,
+          }
+        }
+
+        return {
+          tag,
+          invert: false,
+        }
+      })
+
+      return parsed
+    })
+
+  // filter out undefined from explicit not tags
+  const ORS_filtered = ORS.filter((x) => x !== undefined)
+  if (explicitNotTags.length > 0) {
+    ORS_filtered.forEach((OR, index) => {
+      ORS_filtered[index] = OR.concat(explicitNotTags)
+    })
+  }
+  return ORS_filtered
+}
+
+function shouldTestRunTags(parsedGrepTags, tags = []) {
+  if (!parsedGrepTags.length) {
+    // there are no parsed tags to search for, the test should run
+    return true
+  }
+
+  // now the test has tags and the parsed tags are present
+
+  // top levels are OR
+  const onePartMatched = parsedGrepTags.some((orPart) => {
+    const everyAndPartMatched = orPart.every((p) => {
+      if (p.invert) {
+        return !tags.includes(p.tag)
+      }
+
+      return tags.includes(p.tag)
+    })
+    // console.log('every part matched %o?', orPart, everyAndPartMatched)
+
+    return everyAndPartMatched
+  })
+
+  // console.log('onePartMatched', onePartMatched)
+  return onePartMatched
+}
+
 module.exports = {
-  arraysOverlap,
+  parseTagsGrep,
+  shouldTestRunTags,
   pickTaggedTestsFrom,
   removeEmptyNodes,
   pickTaggedTests,
